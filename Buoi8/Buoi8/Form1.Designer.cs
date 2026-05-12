c95311f85c614a4b7717fdee4415304c9c801615
kannar04 <manhle240406@gmail.com>
2026-05-12 17:20:37 +0700

    commit 8
    add lab_8

---
+++ Buoi8/Buoi8/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Buoi8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboLoaiSP = new System.Windows.Forms.ComboBox();
+            this.txtDGiA = new System.Windows.Forms.TextBox();
+            this.txtTenSP = new System.Windows.Forms.TextBox();
+            this.txtDVT = new System.Windows.Forms.TextBox();
+            this.txtMaSP = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTim = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnADD = new System.Windows.Forms.Button();
+            this.DGV_SP = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_SP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MaSP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DVT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(268, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TenSP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(268, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "D.Gia";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.cboLoaiSP);
+            this.groupBox1.Controls.Add(this.txtDGiA);
+            this.groupBox1.Controls.Add(this.txtTenSP);
+            this.groupBox1.Controls.Add(this.txtDVT);
+            this.groupBox1.Controls.Add(this.txtMaSP);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(24, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(700, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(472, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ma loai SP";
+            // 
+            // cboLoaiSP
+            // 
+            this.cboLoaiSP.FormattingEnabled = true;
+            this.cboLoaiSP.Location = new System.Drawing.Point(536, 57);
+            this.cboLoaiSP.Name = "cboLoaiSP";
+            this.cboLoaiSP.Size = new System.Drawing.Size(158, 21);
+            this.cboLoaiSP.TabIndex = 8;
+            // 
+            // txtDGiA
+            // 
+            this.txtDGiA.Location = new System.Drawing.Point(340, 58);
+            this.txtDGiA.Name = "txtDGiA";
+            this.txtDGiA.Size = new System.Drawing.Size(55, 20);
+            this.txtDGiA.TabIndex = 7;
+            // 
+            // txtTenSP
+            // 
+            this.txtTenSP.Location = new System.Drawing.Point(340, 20);
+            this.txtTenSP.Name = "txtTenSP";
+            this.txtTenSP.Size = new System.Drawing.Size(128, 20);
+            this.txtTenSP.TabIndex = 6;
+            // 
+            // txtDVT
+            // 
+            this.txtDVT.Location = new System.Drawing.Point(73, 59);
+            this.txtDVT.Name = "txtDVT";
+            this.txtDVT.Size = new System.Drawing.Size(128, 20);
+            this.txtDVT.TabIndex = 5;
+            // 
+            // txtMaSP
+            // 
+            this.txtMaSP.Location = new System.Drawing.Point(73, 20);
+            this.txtMaSP.Name = "txtMaSP";
+            this.txtMaSP.Size = new System.Drawing.Size(128, 20);
+            this.txtMaSP.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtTim);
+            this.groupBox2.Controls.Add(this.btnSearch);
+            this.groupBox2.Controls.Add(this.btnLoad);
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Controls.Add(this.btnEdit);
+            this.groupBox2.Controls.Add(this.btnADD);
+            this.groupBox2.Location = new System.Drawing.Point(24, 148);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(700, 100);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // txtTim
+            // 
+            this.txtTim.Location = new System.Drawing.Point(14, 64);
+            this.txtTim.Name = "txtTim";
+            this.txtTim.Size = new System.Drawing.Size(383, 20);
+            this.txtTim.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(429, 61);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Tìm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(429, 19);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "LOAD";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(283, 19);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(146, 19);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnADD
+            // 
+            this.btnADD.Location = new System.Drawing.Point(14, 19);
+            this.btnADD.Name = "btnADD";
+            this.btnADD.Size = new System.Drawing.Size(75, 23);
+            this.btnADD.TabIndex = 0;
+            this.btnADD.Text = "Thêm";
+            this.btnADD.UseVisualStyleBackColor = true;
+            this.btnADD.Click += new System.EventHandler(this.btnADD_Click);
+            // 
+            // DGV_SP
+            // 
+            this.DGV_SP.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_SP.Location = new System.Drawing.Point(18, 269);
+            this.DGV_SP.Name = "DGV_SP";
+            this.DGV_SP.Size = new System.Drawing.Size(700, 150);
+            this.DGV_SP.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 450);
+            this.Controls.Add(this.DGV_SP);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_SP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cboLoaiSP;
+        private System.Windows.Forms.TextBox txtDGiA;
+        private System.Windows.Forms.TextBox txtTenSP;
+        private System.Windows.Forms.TextBox txtDVT;
+        private System.Windows.Forms.TextBox txtMaSP;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtTim;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnADD;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView DGV_SP;
+    }
+}
+
+
---
+++ Buoi8/Buoi8/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buoi8
+{
+    public partial class Form1 : Form
+    {
+        QLBHEntities db;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadSanPham()
+        {
+            QLBHEntities db = new QLBHEntities();
+            var listSP = (from sp in db.SanPhams
+                          select new
+                          {
+                              MaSP = sp.MaSP,
+                              TenSP = sp.TenSP,
+                              DVT = sp.DVTinh,
+                              DonGia = sp.DonGia,
+                              MaLoai = sp.MaLoai,
+                              TenLoai = sp.LoaiSanPham.TenLoai,
+                          }).ToList();
+
+            DGV_SP.DataSource = listSP;
+        }
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadSanPham();
+            DataBiding();
+        }
+
+        internal void DataBiding()
+        {
+            txtMaSP.DataBindings.Add(new Binding("Text", DGV_SP.DataSource, "MaSP", true, DataSourceUpdateMode.Never));
+            txtTenSP.DataBindings.Add(new Binding("Text", DGV_SP.DataSource, "TenSP", true, DataSourceUpdateMode.Never));
+            txtDVT.DataBindings.Add(new Binding("Text", DGV_SP.DataSource, "DVT", true, DataSourceUpdateMode.Never));
+            txtDGiA.DataBindings.Add(new Binding("Text", DGV_SP.DataSource, "DonGia", true, DataSourceUpdateMode.Never));
+            cboLoaiSP.DataBindings.Add(new Binding("SelectedValue", DGV_SP.DataSource, "MaLoai", true, DataSourceUpdateMode.Never));
+        }
+
+        private void InsertSanPham()
+        {
+            using (QLBHEntities db = new QLBHEntities())
+            {
+                SanPham sp = new SanPham();
+                sp.MaSP = txtMaSP.Text;
+                sp.TenSP = txtTenSP.Text;
+                sp.DVTinh = txtDVT.Text;
+                sp.DonGia = int.Parse(txtDGiA.Text);
+                sp.MaLoai = cboLoaiSP.SelectedValue.ToString();
+
+                db.SanPhams.Add(sp);
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Khong Insert được", "Insert Error", MessageBoxButtons.OK);
+                }
+            }    
+        }
+
+        private void btnADD_Click(object sender, EventArgs e)
+        {
+            InsertSanPham();
+            LoadSanPham();
+        }
+
+        private void LoadLoaiSPCombox()
+        {
+            QLBHEntities db = new QLBHEntities();
+            var listLoaiSP = (from lsp in db.LoaiSanPhams
+                              select new
+                              {
+                                  MaLoai = lsp.MaLoai,
+                                  TenLoai = lsp.TenLoai,
+                              }).ToList();
+            cboLoaiSP.DataSource = listLoaiSP;
+            cboLoaiSP.DisplayMember = "TenLoai";
+            cboLoaiSP.ValueMember = "MaLoai";
+        }
+
+        private void UpdateSanPham()
+        {
+            QLBHEntities db = new QLBHEntities();
+
+            SanPham sp = db.SanPhams.Find(txtMaSP.Text);
+            if (sp != null)
+            {
+                sp.TenSP = txtTenSP.Text;
+                sp.DVTinh = txtDVT.Text;
+                sp.DonGia = int.Parse(txtDGiA.Text);
+                sp.MaLoai = cboLoaiSP.SelectedValue.ToString();
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Khong Update được", "Update Error", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            UpdateSanPham();
+            LoadSanPham();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadLoaiSPCombox();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            QLBHEntities db = new QLBHEntities();
+            db.Dispose();
+        }
+
+        private void XoaSanPham()
+        {
+            QLBHEntities db = new QLBHEntities();
+            
+            var sp = db.SanPhams.Find(txtMaSP.Text);
+            if (sp != null)
+            {
+                db.SanPhams.Remove(sp);
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Khong Xoa được", "Delete Error", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            XoaSanPham();
+            LoadSanPham();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string input = txtTim.Text.Trim().ToLower();
+
+            QLBHEntities db = new QLBHEntities();
+            var listSP = (from sp in db.SanPhams
+                          where sp.TenSP.ToLower().Contains(input)
+                          select new
+                          {
+                              MaSP = sp.MaSP,
+                              TenSP = sp.TenSP,
+                              DVT = sp.DVTinh,
+                              DonGia = sp.DonGia,
+                              MaLoai = sp.MaLoai,
+                              TenLoai = sp.LoaiSanPham.TenLoai,
+                          }).ToList();
+
+            DGV_SP.DataSource = listSP;
+        }
+    }
+}
